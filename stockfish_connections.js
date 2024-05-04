dc03c24c8fbd1c82b3c8660854080f9312a45e98
NoobCoder0991 <134902286+NoobCoder0991@users.noreply.github.com>
2024-05-04 22:40:46 +0530

    Update stockfish_connections.js
    Hell

--- stockfish_connections.js
+++ stockfish_connections.js
@@ -1,5 +1,5 @@
 // const { spawn } = require("child_process");
-// const helper_functions = require("./helper_functions");
+const helper_functions = require("./helper_functions");
 
 // async function stockfish_ouput(fenPosition, depth) {
 //   return new Promise((resolve, reject) => {
@@ -137,52 +137,53 @@
 //   };
 // }
 
-// function handleStockfishConnections(io, app) {
-//   var connected = [];
-//   io.of("/play").on("connection", (socket) => {
-//     socket.on("play-stockfish-request", (data) => {
-//       const url = helper_functions.generateURL(8);
-//       app.get("/play/stockfish/" + url, (req, res) => {
-//         res.sendFile(
-//           "C:/Users/shafa/Desktop/BIG_FOLDER/Chess Pro Test - 4/public/src/HTML_Files/stockfish.html"
-//         );
-//       });
-//       socket.emit("play-stockfish-responce", { url: "/play/stockfish/" + url });
-//     });
-//   });
+function handleStockfishConnections(io, app) {
+  return;
+  var connected = [];
+  io.of("/play").on("connection", (socket) => {
+    socket.on("play-stockfish-request", (data) => {
+      const url = helper_functions.generateURL(8);
+      app.get("/play/stockfish/" + url, (req, res) => {
+        res.sendFile(
+          "C:/Users/shafa/Desktop/BIG_FOLDER/Chess Pro Test - 4/public/src/HTML_Files/stockfish.html"
+        );
+      });
+      socket.emit("play-stockfish-responce", { url: "/play/stockfish/" + url });
+    });
+  });
 
-//   const stockfishNamespace = io.of("/play/stockfish");
-//   stockfishNamespace.on("connection", (socket) => {
-//     socket.on("fetch-data-request-stockfish", (data) => {
-//       let match = connected.find((obj) => obj.url == data.url);
-//       if (match) {
-//         socket.emit("fetch-data-responce-stockfish", match.game_details);
-//       } else {
-//         connected.push({ socket: socket, url: data.url });
-//         socket.emit("fetch-data-responce-stockfish", {});
-//       }
-//     });
+  const stockfishNamespace = io.of("/play/stockfish");
+  stockfishNamespace.on("connection", (socket) => {
+    socket.on("fetch-data-request-stockfish", (data) => {
+      let match = connected.find((obj) => obj.url == data.url);
+      if (match) {
+        socket.emit("fetch-data-responce-stockfish", match.game_details);
+      } else {
+        connected.push({ socket: socket, url: data.url });
+        socket.emit("fetch-data-responce-stockfish", {});
+      }
+    });
 
-//     socket.on("save-board-data", (data) => {
-//       const requestData = JSON.parse(data);
-//       let match = connected.find((obj) => obj.url == requestData.url);
-//       if (match) {
-//         match.game_details = requestData;
-//       }
-//     });
+    socket.on("save-board-data", (data) => {
+      const requestData = JSON.parse(data);
+      let match = connected.find((obj) => obj.url == requestData.url);
+      if (match) {
+        match.game_details = requestData;
+      }
+    });
 
-//     socket.on("fetch-move", (data) => {
-//       const fen = data.fen,
-//         depth = data.depth;
-//       stockfish_ouput(fen, depth)
-//         .then((data) => {
-//           socket.emit("fetch-move-responce", data);
-//         })
-//         .catch((err) => {
-//           console.log("error occured");
-//         });
-//     });
-//   });
-// }
+    socket.on("fetch-move", (data) => {
+      const fen = data.fen,
+        depth = data.depth;
+      stockfish_ouput(fen, depth)
+        .then((data) => {
+          socket.emit("fetch-move-responce", data);
+        })
+        .catch((err) => {
+          console.log("error occured");
+        });
+    });
+  });
+}
 
-// module.exports = { handleStockfishConnections };
+module.exports = { handleStockfishConnections };
